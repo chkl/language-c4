9b0fb735069af5f0bd179ce82beb370cb6e8b7a2
Christian Klinger <io@klinch.de>
2017-10-28 13:42:53 +0200

    call lexer from main

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,50 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Main where
 
+import           System.Environment
+
+import           Data.List          (intercalate)
+import           Text.Parsec.Pos
+import           Text.Parsec.String
+
+import           Lexer
+
+
+--------------------------------------------------------------------------------
+-- TODO: This should probably go into its own file
+class PrettyPrint a where
+  prettyPrint :: a -> String
+
+instance PrettyPrint SourcePos where
+  prettyPrint s = intercalate ":" [sourceName s, show $ sourceLine s, show $ sourceColumn s]
+
+instance PrettyPrint Token where
+  prettyPrint (Keyword k)      = "keyword " ++ k
+  prettyPrint (Identifier i)   = "identifier " ++ i
+  prettyPrint (DecConstant n)  = "constant "  ++ show n
+  prettyPrint (CharConstant c) = "??" -- TODO how is this called?
+  prettyPrint (StringLit s)    = "string-literal " ++ show s
+  prettyPrint (Punctuator s)   = "punctuator " ++ s
+
+
+instance PrettyPrint [(Token, SourcePos)] where
+  prettyPrint = unlines . map prettyPrint'
+    where prettyPrint' (t,p) = prettyPrint p ++ " " ++ prettyPrint t
+--------------------------------------------------------------------------------
+
+tokenize :: String -> IO ()
+tokenize filename = do
+  res <- parseFromFile lexer filename
+  case res of
+    Left err     -> print err
+    Right tokens -> putStr $ prettyPrint tokens
+
+showHelp :: IO ()
+showHelp = print "Available options: --tokenize and --help"
 
 main :: IO ()
-main = putStrLn "TODO: Implement Compiler"
+main = do
+  args <- getArgs
+  case args of
+    ["--tokenize", filename] -> tokenize filename
+    _                        -> showHelp
