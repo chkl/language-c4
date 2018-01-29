63893a3c4d30099794aafb95145cd8824daddbb9
fstutz <fstutz@mpi-inf.mpg.de>
2018-01-29 16:54:03 +0100

    wip

--- src/Analysis.hs
+++ src/Analysis.hs
@@ -18,7 +18,7 @@ import           Control.Monad        (forM, when)
 import           Control.Monad.State
 import           Control.Monad.Writer
 import qualified Data.Map.Strict      as Map
-import           Text.Megaparsec.Pos  (SourcePos)
+import           Text.Megaparsec.Pos  (SourcePos, sourceLine, sourceColumn, unPos)
 
 import           Ast.SemAst
 import           Ast.SynAst
@@ -66,11 +66,14 @@ data SemanticError = UndeclaredName !SourcePos !Ident
                    | NoPointer SourcePos
 
 instance Show SemanticError where
-  show (UndeclaredName p i)      = (show p) ++ ": " <> "undeclared name " <> (show i)
-  show (AlreadyDeclaredName p i) = (show p) ++ ": " <> "name already declared " <> (show i)
-  show (TypeMismatch p l r)      = (show p) ++ ": " <> "type mismatch: expected " <> (show l) <>
-                                   " but got " <> (show r)
-  show (NoPointer p)             = (show p) ++ ": " <> "expects a pointer"
+  show (UndeclaredName p i)      = (nicePos p) ++ " : " <> "undeclared name " <> (show i)
+  show (AlreadyDeclaredName p i) = (nicePos p) ++ " : " <> "name already declared " <> (show i)
+  show (TypeMismatch p l r)      = (nicePos p) ++ " : " <> "type mismatch: expected type: " <> (show l)
+                                   <> "; actual type: " <> (show r)
+  show (NoPointer p)             = (nicePos p) ++ " : " <> "expects a pointer"
+
+nicePos :: SourcePos -> String
+nicePos p = (show (unPos (sourceLine p))) <> ":" <> (show (unPos (sourceColumn p))) 
   
 -- | runs an analysis in a copy of the current scope without modifying the
 -- original scope, but retains the errors.
